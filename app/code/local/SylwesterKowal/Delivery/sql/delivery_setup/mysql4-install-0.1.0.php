c2b957cc26c910afcf08bbed5e12692623c7fec0
SK <sylwester.kowal@21w.pl>
2016-10-13 11:30:14 +0200

    Uproszczona Instalacja Kodu Sklepu

--- app/code/local/SylwesterKowal/Delivery/sql/delivery_setup/mysql4-install-0.1.0.php
+++ app/code/local/SylwesterKowal/Delivery/sql/delivery_setup/mysql4-install-0.1.0.php
@@ -13,29 +13,7 @@ $installer->run($sql);
 
 $host = parse_url(Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB));
 
-$page = '/install';
-$url = 'http://delivery.21order.com' . $page;
-
-$key = md5(crc32($host), true);
-$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
-$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-$ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $host, MCRYPT_MODE_CBC, $iv);
-$ciphertext = $iv . $ciphertext;
-$ciphertext_base64 = base64_encode($ciphertext);
-
-
-$data['data'] = $ciphertext_base64;
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$code = curl_exec($ch);
-curl_close($ch);
-
-
-Mage::getConfig()->saveConfig('deliverysection/settings/code', $code, 'default', '');
+Mage::getConfig()->saveConfig('deliverysection/settings/code', crc32($host), 'default', '');
 
 $installer->endSetup();
 	 
